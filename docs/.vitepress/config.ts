aacac4413f8ffec9831e2311a1e04682485c8ae7
Lyoko <519052532@qq.com>
2024-07-31 14:15:48 +0800

    docs: base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
+  base: '/vixt',
   title: 'Vixt',
   description: 'Trying to implement nuxt\'s features via vite plugin',
   themeConfig: {
