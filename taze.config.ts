1c84aed58a24d3a4563c98c7d4b78e7d8134061e
Lyoko <519052532@qq.com>
2025-01-09 10:28:37 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'taze'
 
 export default defineConfig({
   // ignore packages from bumping
-  exclude: [],
+  exclude: ['@vitejs/plugin-vue', '@vitejs/plugin-vue-jsx'],
 })
