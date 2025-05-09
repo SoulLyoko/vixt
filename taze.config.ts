11d20b7fb29aa9e7af7e6fb2f6c50cb3f8e22763
Lyoko <519052532@qq.com>
2025-05-09 10:48:08 +0800

    chore: lock unocss v66.0.0

--- taze.config.ts
+++ taze.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
   exclude: [
     'pnpm',
     'unplugin-vue-components', // latest version uses `tinyglobby` and gets wrong files order
+    'unocss,@unocss/*', // v66.1.0 hot reload fail `could not find "src/__uno.css"`
   ],
 })
