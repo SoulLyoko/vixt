0947638c6033a2785901fd361a0c0c69dbed6056
SoulLyoko <48344443+SoulLyoko@users.noreply.github.com>
2025-05-22 03:38:11 +0000

    Deploying to gh-pages from @ SoulLyoko/vixt@14bfc6b59b83b97ba55f37646e8f38ec0282988c 🚀

---
+++ assets/api_vixt.md.BlQdCIED.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as a,o as n,ag as p}from"./chunks/framework.DINf6-p9.js";const g=JSON.parse('{"title":"从 vixt 导入","description":"","frontmatter":{},"headers":[],"relativePath":"api/vixt.md","filePath":"api/vixt.md"}'),l={name:"api/vixt.md"};function t(h,s,e,k,d,E){return n(),a("div",null,s[0]||(s[0]=[p("",10)]))}const o=i(l,[["render",t]]);export{g as __pageData,o as default};
