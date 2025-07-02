beba5f1a9bb52213a73dc3688f8600d3b89f6f99
Lyoko <519052532@qq.com>
2025-07-02 09:49:44 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   // ignore packages from bumping
   exclude: [
     'pnpm',
-    'unplugin-vue-components', // latest version uses `tinyglobby` and gets wrong files order
+    'unplugin-vue-components', // v28.1.0 uses `tinyglobby` and gets wrong files order
     'unocss,@unocss/*', // v66.1.0 hot reload fail `could not find "src/__uno.css"`
   ],
 })
