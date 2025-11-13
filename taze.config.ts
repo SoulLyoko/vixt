a8d23ae9d9900a0835f756314ddfd19d7050c00a
Lyoko <519052532@qq.com>
2025-11-13 15:09:36 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     'unplugin-vue-components', // v28.1.0 uses `tinyglobby` and gets wrong files order (https://github.com/unplugin/unplugin-vue-components/issues/831)
     // 'unocss,@unocss/*', // v66.1.0 hot reload fail `could not find "src/__uno.css"`
     '@dcloudio/*', // ignore alpha version
-    '@vueuse/*', // drops `resolveUnref` and `resolveRef` but `@uni-helper/uni-use` needed (https://github.com/uni-helper/uni-use/pull/58)
+    '@vueuse/*', // v14.0.0 drops `resolveUnref` and `resolveRef` but `@uni-helper/uni-use` needed (https://github.com/uni-helper/uni-use/pull/58)
   ],
 })
