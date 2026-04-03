7c644721e2afd36e8d6c4129cde47f0ebb5aecff
Lyoko <519052532@qq.com>
2026-04-03 17:56:28 +0800

    chore!: update vite to 8, update typescript to 6
    BREAKING CHANGE: update vite to 8, update typescript to 6

--- taze.config.ts
+++ taze.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   exclude: [
     'pnpm',
     'unplugin-vue-components', // v28.1.0 uses `tinyglobby` and gets wrong files order (https://github.com/unplugin/unplugin-vue-components/issues/831)
-    // 'unocss,@unocss/*', // v66.1.0 hot reload fail `could not find "src/__uno.css"`
+    '@uni-helper/vite-plugin-uni-components', // same as `unplugin-vue-components`
+    // 'unocss,@unocss/*', // v66.1.0 hot reload fail `could not find "src/__uno.css"` (fixed)
     '@dcloudio/*', // ignore alpha version
     '@vueuse/*', // v14.0.0 drops `resolveUnref` and `resolveRef` but `@uni-helper/uni-use` needed (https://github.com/uni-helper/uni-use/pull/58)
   ],
