da743554209041426c600dcd715ef4b5da6ee4c7
Lyoko <519052532@qq.com>
2026-05-22 17:01:29 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   exclude: [
     '@dcloudio/*', // ignore alpha version
     '@uni-helper/vite-plugin-uni-components', // same as `unplugin-vue-components`
-    'bumpp', // v11.0.0 files option bug (https://github.com/antfu-collective/bumpp/issues/119)
     'unplugin-vue-components', // v28.1.0 uses `tinyglobby` and gets wrong files order (https://github.com/unplugin/unplugin-vue-components/issues/831)
   ],
 })
