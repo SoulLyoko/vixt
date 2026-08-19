75298b11da12f49abcb36b25ee7f90d30d2438ef
Lyoko <519052532@qq.com>
2026-08-19 10:51:17 +0800

    chore: up deps

--- taze.config.ts
+++ taze.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'taze'
 
 export default defineConfig({
+  githubActions: false,
   // ignore packages from bumping
   exclude: [
     '@dcloudio/*', // ignore alpha version
     '@uni-helper/vite-plugin-uni-components', // same as `unplugin-vue-components`
     '@uni-helper/vite-plugin-uni-pages', // v0.4.0 route block has been deprecated
+    'typescript', // vue-tsc not yet support ts7
     'unplugin-vue-components', // v28.1.0 uses `tinyglobby` and gets wrong files order (https://github.com/unplugin/unplugin-vue-components/issues/831)
+    'actions/*',
   ],
 })
